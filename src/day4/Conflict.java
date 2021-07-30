56650223004d660394bb7068459f99a7a905827a
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-30 09:48:02 -0500

    collection2 edit is done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -7,5 +7,17 @@ public class Conflict {
     // I can type anything I want
 
     // this is collection branch content
+    //MORE WORK ON COLLECTION2
+
 
 }
+/*
+#### **Task 2**
+1. create a new branch called `collection2`
+2. Add a comment in the class `Conflict` with this message `MORE WORK ON COLLECTION2`
+3. Commit this change
+4. Checkout `master`
+5. Add a different comment in the class `Conflict` with `This is the change collection2 branch does not know about`
+6. Commit this change into master
+7. Now `merge` the `collection2` branch and boom! Conflict!!
+ */
